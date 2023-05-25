cb460b01b9438e6688331d430185ce0679656c21
ReymartNarvaez <reymart_narvaez@hotmail.com>
2023-05-25 11:58:06 +0200

    Add GeneratedValue for id

--- src/main/java/com/rosegarden/entity/ProductEntity.java
+++ src/main/java/com/rosegarden/entity/ProductEntity.java
@@ -8,6 +8,7 @@ import javax.persistence.*;
 public class ProductEntity extends PanacheEntityBase {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private double price;
