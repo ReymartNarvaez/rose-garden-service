c92be744b4d8a5522772ecdec224b4a649e5db0d
ReymartNarvaez <reymart_narvaez@hotmail.com>
2023-05-25 11:57:01 +0200

    Add configuration for product.sql

---
+++ src/main/resources/products.sql
@@ -0,0 +1,17 @@
+INSERT INTO products(name, price)
+VALUES ('Pot Original', '350');
+
+INSERT INTO products(name, price)
+VALUES ('Pot Legend', '550');
+
+INSERT INTO products(name, price)
+VALUES ('Pot DarkStyle', '450');
+
+INSERT INTO products(name, price)
+VALUES ('Pot Style', '250');
+
+INSERT INTO products(name, price)
+VALUES ('Pot LightStyle', '150');
+
+INSERT INTO products(name, price)
+VALUES ('Pot', '90');
